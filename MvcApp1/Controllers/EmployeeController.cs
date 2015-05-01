88b93b82aecef447d27f34aed51c4bb2ce100ba5
s1101137205 <qaz830718@yahoo.com.tw>
2015-05-01 16:53:03 +0800

    frist

---
+++ MvcApp1/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApp1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEmployeeService EmployeeService { get; set; }
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeService.GetAllEmployees();
+        }
+
+        [HttpGet]
+        [ActionName("byId")]
+        public Employee GetEmployeeById(string id)
+        {
+            var employee = EmployeeService.GetEmployeeById(id);
+
+            if (employee == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return employee;
+        }
+
+        [HttpGet]
+        [ActionName("byName")]
+        public Employee GetEmployeeByName(string name)
+        {
+            var employee = EmployeeService.GetEmployeeByName(name);
+
+            if (employee == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return employee;
+        }
+
+
+    }
+}
---
+++ MyWebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyWebApp.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEmployeeService EmployeeService { get; set; }
+
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeService.GetAllEmployees();
+        }
+    }
+}
